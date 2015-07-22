8761b8c25629f7cb0ce2239c0fd85a89113d58ae
Tristan Sloughter <t@crashfast.com>
2015-07-22 14:21:48 -0500

    update host url

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # meaningful, we use this information when generating URLs.
 config :chat, Chat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com"]
+  url: [host: System.get_env("HOST"), port: 80]
 
 # ## SSL Support
 #
